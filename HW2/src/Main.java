0116db24846f25e6620d58d29b8355113afd7b1c
maptupocob <30635657+maptupocob@users.noreply.github.com>
2020-02-11 13:11:26 +0300

    add and delete Item methods done

--- HW2/src/Main.java
+++ HW2/src/Main.java
@@ -1,13 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] myArr = new int[1_000_000];
+        int[] bigArr = new int[1_000_000];
+        int[] lilArr = {1,2,3,4,5,6,7};
 
-        for (int i = 0; i < myArr.length ; i++) {
-            myArr[i] = (int) (Math.random()* 1_000_000);
+        for (int i = 0; i < bigArr.length ; i++) {
+            bigArr[i] = (int) (Math.random()* 1_000_000);
         }
 
-        MyArrayUtil.searchItem(myArr, 12);
+        int index =  MyArrayUtil.searchItem(bigArr, 12);
+        if (index == -1){
+            System.out.printf("Search item not found\n");
+        }else{
+            System.out.printf("Index of search item: %,6d\n", index);
+        }
+
+        System.out.println("Init array state:");
+        MyArrayUtil.displayArray(lilArr);
+        try {
+            MyArrayUtil.deleteItem(lilArr, 3);
+        } catch (ItemNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("After deleting element:");
+        MyArrayUtil.displayArray(lilArr);
+
+        MyArrayUtil.addItem(lilArr, 5, 4);
+        System.out.println("After adding element:");
+        MyArrayUtil.displayArray(lilArr);
+
+
+
+
 
 
     }
--- HW2/src/MyArrayUtil.java
+++ HW2/src/MyArrayUtil.java
@@ -1,20 +1,43 @@
 public class MyArrayUtil {
     public static int searchItem(int[] arr, int searchItem) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == searchItem){
-                System.out.printf("Index of search item: %,6d", i);
+            if (arr[i] == searchItem) {
                 return i;
             }
         }
-        System.out.printf("Search item not found");
-        return  -1;
+        return -1;
     }
 
-    public static void addItem(int[] arr, int newItem) {
-
+    // Добавление элемента в массив со сдвигом вправо и выталкиванием последнего элемента
+    public static void addItem(int[] arr, int newItem, int index) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = newItem;
     }
 
-    public static void deleteItem(int[] arr, int deletedItem) {
+    //Удаление элемента из массива со сдвигом влево и занулением последнего элемента
+    public static void deleteItem(int[] arr, int deletedItem) throws ItemNotFoundException {
+        int index = searchItem(arr, deletedItem);
+        if (index == -1) {
+            throw new ItemNotFoundException();
+        } else {
+            for (int i = index; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length -1] = 0;
+        }
+    }
 
+    public static void displayArray(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            sb.append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("]");
+        System.out.println(sb);
     }
 }
